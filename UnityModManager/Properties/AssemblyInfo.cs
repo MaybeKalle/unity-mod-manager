6e644d89307d1744a18c2f7e946d322cf54c9794
Frederik <15572935+MaybeKalle@users.noreply.github.com>
2022-10-30 16:45:03 +0100

    Changed the version info Changed the version information of the Assembly file to represent a newer and modified version

--- UnityModManager/Properties/AssemblyInfo.cs
+++ UnityModManager/Properties/AssemblyInfo.cs
@@ -2,12 +2,12 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnityModManager")]
+[assembly: AssemblyTitle("UnityModManager - MaybeKalle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnityModManager")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("UnityModManager - MaybeKalle")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("6e8f8b45-2a2d-4e5a-8573-3559337e68f3")]
 
-[assembly: AssemblyVersion("0.25.0")]
-[assembly: AssemblyFileVersion("0.25.0")]
+[assembly: AssemblyVersion("0.30.0")]
+[assembly: AssemblyFileVersion("0.30.0")]
